e44dc8e66ff42779ef0f8cf896c3d49f767cbca4
jonghwa <jonghwa0710@naver.com>
2019-02-12 22:24:27 +0900

    boot06 start

---
+++ boot05/src/main/java/org/zerock/Boot05Application.java
@@ -0,0 +1,18 @@
+package org.zerock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Boot05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot05Application.class, args);
+	}
+	
+
+
+}
+
