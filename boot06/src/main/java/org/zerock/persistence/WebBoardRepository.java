cd887d9bd8bbe942e120db486b0523db4334ca87
jonghwa <jonghwa0710@naver.com>
2019-02-13 20:06:47 +0900

    300P 까지

--- boot06/src/main/java/org/zerock/persistence/WebBoardRepository.java
+++ boot06/src/main/java/org/zerock/persistence/WebBoardRepository.java
@@ -2,8 +2,37 @@ package org.zerock.persistence;
 
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.zerock.domain.QWebBoard;
 import org.zerock.domain.WebBoard;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 
 public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard> {
 
+	public default Predicate makePredicate(String type, String keyword) {
+		BooleanBuilder builder = new BooleanBuilder();
+		QWebBoard board = QWebBoard.webBoard;
+
+		// type if ~ else
+
+		if (type == null) {
+			return builder;
+		}
+
+		// bno > 0
+
+		switch (type) {
+		case "t":
+			builder.and(board.title.like("%" + keyword + "%"));
+			break;
+		case "c":
+			builder.and(board.content.like("%" + keyword + "%"));
+			break;
+		case "w":
+			builder.and(board.writer.like("%" + keyword + "%"));
+		}
+
+		return builder;
+	}
 }
--- boot06/src/test/java/org/zerock/WebBoardTests.java
+++ boot06/src/test/java/org/zerock/WebBoardTests.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.WebBoard;
+import org.zerock.persistence.WebBoardRepository;
 
 import lombok.extern.java.Log;
 
@@ -16,12 +21,11 @@ import lombok.extern.java.Log;
 @SpringBootTest
 @Log
 @Commit
-public class WebBoardRepository {
+public class WebBoardTests {
 
 	@Autowired
-	org.zerock.persistence.WebBoardRepository repo;
+	WebBoardRepository repo;
 
-	@Test
 	public void insertBoardDummies() {
 		IntStream.range(0, 300).forEach(i -> {
 			;
@@ -32,5 +36,26 @@ public class WebBoardRepository {
 			board.setWriter("user0" + (i % 10));
 			repo.save(board);
 		});
+	}
+
+	public void testList1() {
+		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), pageable);
+
+		log.info("PAGE: " + result.getPageable());
+		log.info("---------------------------");
+		result.getContent().forEach(board -> log.info("" + board));
+	}
+
+	@Test
+	public void testList2() {
+		Pageable pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+
+		Page<WebBoard> result = repo.findAll(repo.makePredicate("t", "10"), pageable);
+
+		log.info("PAGE: " + result.getPageable());
+		log.info("---------------------------");
+		result.getContent().forEach(board -> log.info("" + board));
 	}
 }
